7606048fcb15664f272002f02230983b3ff2ec26
sakharehiteshh <sakharehiteshh@gmail.com>
2024-04-02 16:27:39 -0400

    Resume Updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
   const mainGif = getImageUrl("hero/HitMemoji2.gif");
   const hoveredGif = getImageUrl("hero/HitMemoji.gif");
   const openResume = () => {
-    window.open('https://drive.google.com/file/d/1QIyhcrvI1zCQv6pBQyQ5x8Y-k2vk6Q8m/view?usp=drive_link', '_blank');
+    window.open('https://drive.google.com/file/d/1LhwOHm4p-6HtVzpZe-z38v_sbGJZEtST/view?usp=drive_link', '_blank');
   };
   
   const [isPaused, setIsPaused] = useState(false);
