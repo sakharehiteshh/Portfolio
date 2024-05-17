f41c3f77bfd3c632e66a3fb87e1b133fefcfe2b4
sakharehiteshh <sakharehiteshh@gmail.com>
2024-05-17 14:07:10 -0400

    Updated projects

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -7,20 +7,27 @@ export const ProjectCard = ({
   project: { title, imageSrc, description, skills, demo, source },
 }) => {
   return (
-    <div className={styles.container}>
-      <img
-        src={getImageUrl(imageSrc)}
-        alt={`Image of ${title}`}
-        className={styles.image}
-      />
-      <h3 className={styles.title}>{title}</h3>
-      <p className={styles.description}>{description}</p>
-      
-      <div className={styles.links}>
-        <a href={source} target="_blank" className={styles.link}>
-        <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-        </a>
+    <div >
+      <div className={styles.container}>
+        <img
+          src={getImageUrl(imageSrc)}
+          alt={`Image of ${title}`}
+          className={styles.image}
+        />
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.description}>{description}</p>
+        
       </div>
+      
+          <div className={styles.links}>
+            <a href={source} target="_blank" className={styles.link}>
+            <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+            </a>
+          </div> 
+      
+
+    
     </div>
+
   );
 };
