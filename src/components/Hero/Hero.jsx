1db324e8968628bd7174e2ab96adb1095421b316
sakharehiteshh <sakharehiteshh@gmail.com>
2024-05-20 18:06:42 -0400

    Updated Contact and Logo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -31,18 +31,18 @@ export const Hero = () => {
       onMouseOver={handleMouseOver}
       onMouseOut={handleMouseOut}
     >
-        <marquee behavior="scroll" direction="up" className={styles.description} scrollamount={isPaused ? 0 : 3}>Hello, I'm Hitesh Vilas Sakhare, a dedicated and passionate software engineer with a Master of Science in Computer Science from the University of Dayton and a Bachelor of Engineering in Electronics Engineering from the University of Mumbai. With a solid foundation in both theoretical concepts and practical applications, I bring a diverse skill set and a wealth of experiences to every project I undertake.
+          <marquee behavior="scroll" direction="up" className={styles.description} scrollamount={isPaused ? 0 : 3}>Hello, I'm Hitesh Vilas Sakhare, a dedicated and passionate software engineer with a Master of Science in Computer Science from the University of Dayton and a Bachelor of Engineering in Electronics Engineering from the University of Mumbai. With a solid foundation in both theoretical concepts and practical applications, I bring a diverse skill set and a wealth of experiences to every project I undertake.
 
-Key Skills,
+            Key Skills,
 
-I specialize in a wide range of programming languages, including Java, Python, JavaScript, and SQL, allowing me to adapt to various project requirements seamlessly. My expertise extends to popular frameworks such as Angular, React, and Django, as well as cloud services like AWS, GCP, and Azure.
+            I specialize in a wide range of programming languages, including Java, Python, JavaScript, and SQL, allowing me to adapt to various project requirements seamlessly. My expertise extends to popular frameworks such as Angular, React, and Django, as well as cloud services like AWS, GCP, and Azure.
 
-What Sets Me Apart is my ability to not only deliver high-quality code but also to think strategically and creatively to solve complex problems. Moreover, my commitment to continuous learning and professional development ensures that I stay updated with the latest technologies and best practices in the industry.
+            What Sets Me Apart is my ability to not only deliver high-quality code but also to think strategically and creatively to solve complex problems. Moreover, my commitment to continuous learning and professional development ensures that I stay updated with the latest technologies and best practices in the industry.
 
-Thank you for taking the time to learn more about me. I look forward to the opportunity to collaborate and contribute to exciting projects that push the boundaries of what's possible in the world of technology.
+            Thank you for taking the time to learn more about me. I look forward to the opportunity to collaborate and contribute to exciting projects that push the boundaries of what's possible in the world of technology.
 
-</marquee>
-</div>
+          </marquee>
+        </div>
           <button onClick={openResume} class={styles.contactBtn}>Resume</button> 
             
           
