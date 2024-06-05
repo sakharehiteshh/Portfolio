e8e41a6b26f21be1d19354b83cb5f765e5918614
sakharehiteshh <sakharehiteshh@gmail.com>
2024-06-04 22:38:05 -0400

    Scroll to top added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Skills } from "./components/Skills/Skills";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Projects } from "./components/Projects/Projects";
+import { ScrollToTop } from "./components/ScrollToTop/ScrollToTop";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Experience />
       <Projects />
       <Contact />
+      <ScrollToTop/>
     </div>
   );
 }
---
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import styles from "./ScrollToTop.module.css";
+import scrollUpImg from "../../../assets/skills/up.gif";
+
+
+
+export const ScrollToTop = () => {
+    
+    const [backToTopButton, setBackToTopButton] = useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll",()=>{
+            window.scrollY > 100 ? setBackToTopButton(true) : setBackToTopButton(false)
+        })
+    }, [])
+
+    const scrollUp = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    }
+
+    return (
+        <div>
+            {backToTopButton && (
+                <button className={styles.scrollTop} style={{
+                    position: "fixed",
+                    bottom: "50px",
+                    right: "50px",
+                    height: "50px",
+                    width: "50px",
+                }} onClick={scrollUp}><img className={styles.scrollTopImg}  src="../../../assets/skills/up.gif"></img></button>
+            )}
+        </div>
+    );
+}
+
